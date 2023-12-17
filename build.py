67fd85b80deba4f3f7c6e3b32c9c6b5cfaa7fb03
boyair <yair.zuriel1@gmail.com>
2023-12-17 18:43:11 +0200

    updated build script to tell user to build by himself in vs2022 instaead of trying to build and failing.

--- build.py
+++ build.py
@@ -1,32 +1,32 @@
 import os
 import sys
 
-print("build options: ")
-print("1: Developer (build in debug mode)")
-print("2: User (build in release mode)")
-print("3: Abort")
-
-valid = False
-while True:
-    choice = input("your choice: ")
-    try:
-        choice = int(choice)
-    except ValueError:
-        print("must enter a number!! try again.")
-        continue
-    if choice > 3 or choice < 1:
-        print("must choose number between 1 and 5. try again")
-    else:
-        break
-
-if choice == 3:
-    print("building process aborted by user.")
-    exit(0)
-
-executablepath = "Debug" if choice == 1 else "Release"
-print("building . . .")
-
 if sys.platform == "linux":
+    print("build options: ")
+    print("1: Developer (build in debug mode)")
+    print("2: User (build in release mode)")
+    print("3: Abort")
+
+    valid = False
+    while True:
+        choice = input("your choice: ")
+        try:
+            choice = int(choice)
+        except ValueError:
+            print("must enter a number!! try again.")
+            continue
+        if choice > 3 or choice < 1:
+            print("must choose number between 1 and 5. try again")
+        else:
+            break
+    
+    if choice == 3:
+        print("building process aborted by user.")
+        exit(0)
+    
+    executablepath = "Debug" if choice == 1 else "Release"
+    print("building . . .")
+    
     if choice == 1:
         # Build Debug configuration on Linux using GNU Make
         os.system("premake5 --os=linux gmake")
@@ -40,17 +40,10 @@ if sys.platform == "linux":
     print(f"if no errors accured the executable can be found in the bin/{executablepath}")
 
 elif sys.platform == "win32":
-    if choice == 1:
-        # Build Debug configuration on Windows using Visual Studio 2022
-        os.system("premake5-windows.exe --os=windows vs2022")
-        os.system("msbuild /p:Configuration=Debug")
-    elif choice == 2:
-        # Build Release configuration on Windows using Visual Studio 2022
-        os.system("premake5-windows.exe --os=windows vs2022")
-        os.system("msbuild /p:Configuration=Release")
+    os.system("premake5-windows.exe --os=windows vs2022")
 
-    print("done building.")
-    print("if no errors accured the executable can be found in the bin/Debug or bin/Release according to your build.")
+    print("done creating solution")
+    print("if no errors accured you can open game.sln file with visual studio 2022 and build the project from there.")
 
 else:
     print("Operating system not supported...\n this application only support windows and linux :(")
